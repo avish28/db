668472d686ff41aa22211b412a6031000b1788c2
avish28 <modiavish0@gmail.com>
2020-09-02 10:38:22 +0530

    models have been created

--- company/home/models.py
+++ company/home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class employee(models.Model):
+    emp_fname=models.CharField(max_length=10)
+    emp_lname=models.CharField(max_length=10)
+    emp_mob=models.CharField(max_length=10,unique=True)
+    emp_dept=models.ManyToManyField('department',related_name='dep_emp')
+
+    def __str__(self):
+        return f"Employee:{self.emp_fname}  {self.emp_lname}"
+
+
+class department(models.Model):
+    dept_name=models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Department:{self.dept_name}"
